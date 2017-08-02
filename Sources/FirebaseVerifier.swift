d3d7e9dc036bb6451235476506cadc4ff54a3e14
Masayuki Ono <mono0926@gmail.com>
2017-08-02 18:15:02 +0900

    Support linux

--- Sources/FirebaseVerifier.swift
+++ Sources/FirebaseVerifier.swift
@@ -75,7 +75,7 @@ public struct FirebaseVerifier {
         let response = try String(contentsOf: URL(string: "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")!,
                                   encoding: .utf8)
 
-        guard let keys = response.toJSON() as? NSDictionary, let publicKey = keys[keyIdentifier] as? String else {
+        guard let keys = response.toJSON() as? Dictionary<String, String>, let publicKey = keys[keyIdentifier] else {
             let message = "Firebase ID token has 'kid'(\(keyIdentifier)) claim which does not correspond to a known public key. Most likely the ID token is expired, so get a fresh token from your client app and try again. \(verifyIdTokenDocsMessage)"
             throw VerificationError(type: .notFound(key: "public key"), message: message)
         }
